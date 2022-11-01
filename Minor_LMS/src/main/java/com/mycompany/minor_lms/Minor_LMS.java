5e10786857f560025e100060641fb6cfe72acecf
TXM-DOOM <divyamk2025@gmail.com>
2022-11-01 10:08:18 +0530

    Added Minor-LMS, icons and Kgradient Panel Master

--- Minor_LMS/src/main/java/com/mycompany/minor_lms/Minor_LMS.java
+++ Minor_LMS/src/main/java/com/mycompany/minor_lms/Minor_LMS.java
@@ -3,13 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.lms_register;
+package com.mycompany.minor_lms;
 
 /**
  *
  * @author Divyam
  */
-public class LMS_REGISTER {
+public class Minor_LMS {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is a repo for our Minor 1 project in which We are making a Library Managment System using encryption for authentication.
 The main language used in our project will be Java.
 The main focus will be on using encryption algorithms and using DS based algorithms
+# For Gradient File
+Unzip the KGradient zip file
